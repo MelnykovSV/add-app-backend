ad842c045d6ce7c1194cd14a6e34938b7376cee2
MelnykovSV <svmelnykov@gmail.com>
2024-04-22 21:37:42 +0300

    added adds filtration by coordinates

--- src/controllers/adds/getAdds.ts
+++ src/controllers/adds/getAdds.ts
@@ -1,10 +1,19 @@
 const { Add } = require('../../models/add');
-const { createResponse } = require('../../helpers');
+const { createResponse, HttpError } = require('../../helpers');
 
 type IExtendedRequest = import('../../interfaces').IExtendedRequest;
 
 const getAdds = async (req: IExtendedRequest, res: Express.Response) => {
-  const data = await Add.find({}, '-createdAt -updatedAt');
+  const { minLat, maxLat, minLon, maxLon } = req.query;
+
+  if (!minLat || !maxLat || !minLon || !maxLon) {
+    throw HttpError(400, 'Provide Lan and Lon range');
+  }
+
+  const data = await Add.find(
+    { lat: { $gt: minLat, $lt: maxLat }, lon: { $gt: minLon, $lt: maxLon } },
+    '-createdAt -updatedAt',
+  );
 
   createResponse(res, 200, 'Adds', {
     adds: data,
