960def47e4ecf0ca75d82f53ffe2c014bc3d5eca
MelnykovSV <svmelnykov@gmail.com>
2024-04-25 04:20:15 +0300

    added search functionality

--- src/controllers/listings/getListings.ts
+++ src/controllers/listings/getListings.ts
@@ -4,16 +4,25 @@ const { createResponse, HttpError } = require('../../helpers');
 type IExtendedRequest = import('../../interfaces').IExtendedRequest;
 
 const getListings = async (req: IExtendedRequest, res: Express.Response) => {
-  const { minLat, maxLat, minLon, maxLon } = req.query;
+  const { minLat, maxLat, minLon, maxLon, searchQuery } = req.query;
 
   if (!minLat || !maxLat || !minLon || !maxLon) {
     throw HttpError(400, 'Provide Lan and Lon range');
   }
 
-  const data = await Listing.find(
-    { lat: { $gt: minLat, $lt: maxLat }, lon: { $gt: minLon, $lt: maxLon } },
-    '-createdAt -updatedAt',
-  );
+  const filter: any = {
+    lat: { $gt: minLat, $lt: maxLat },
+    lon: { $gt: minLon, $lt: maxLon },
+  };
+
+  if (searchQuery) {
+    filter.$or = [
+      { name: { $regex: searchQuery, $options: 'i' } },
+      { description: { $regex: searchQuery, $options: 'i' } },
+    ];
+  }
+
+  const data = await Listing.find(filter, '-createdAt -updatedAt');
 
   createResponse(res, 200, 'Listings', {
     listings: data,
